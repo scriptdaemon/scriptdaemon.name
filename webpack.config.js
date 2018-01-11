0c44800c5e61eabf7d3af8672125712891c382aa
Kenneth Williams <scriptdaemon@gmail.com>
2018-01-10 21:01:00 -0800

    chore(package): Remove `css-loader` rule
    All styling is automatically processed via the `vue-loader` when
    imported within a Vue.js component.

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: [
-          'vue-style-loader',
-          'css-loader'
-        ]
-      },
-      {
         test: /\.vue$/,
         loader: 'vue-loader'
       }
